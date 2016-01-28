ced8f896cc6f8dea01f393c0bc7ad5d327a84e7b
zerokullneo <zerokullneo@debian-msi>
2016-01-28 17:55:06 +0100

    Primera versión de la Hoja1 de problemas

---
+++ src/hoja1/Hoja1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja1;
+
+/**
+ *
+ * @author Jose Manuel Barba Gonzalez <zk at wordpress.com>
+ */
+public class Hoja1
+{
+
+     /**
+      * @param args the command line arguments
+      */
+     public static void main(String[] args)
+     {
+	// TODO code application logic here
+     }
+     
+}
