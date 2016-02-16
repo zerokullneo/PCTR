bc1b61d0b82b113dd338840900a59b5a3e40760e
Jose M Barba Gonzalez <zerokullneo@localhost.localdomain>
2016-02-16 10:27:11 +0100

    Modificaicón de la práctica 11
    Corrección de errores 1.

--- PCTR-Practicas/Practica11/src/practica11/cPiMonteCarlo.java
+++ PCTR-Practicas/Practica11/src/practica11/cPiMonteCarlo.java
@@ -1,11 +1,34 @@
 /**
- * @author David Moreno Del Valle
+ * Copyright (C) 2016 Jose Manuel Barba Gonzalez <zk at wordpress.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+package practica11;
+
+/**Fichero cPiMonteCarlo.java
+ * @author Jose Manuel Barba Gonzalez
+ * @version 1.0
+ * Programacion Concurrente y de Tiempo Real
+ * Area de CC. de la Computacion e I.A.
+ */
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.util.*;
-
+/**Descripcion
+ * 
+ */
 public class cPiMonteCarlo {
 
     public static void main(String[] args)
@@ -19,13 +42,13 @@ public class cPiMonteCarlo {
 
     	while(exit==true)
     	{
-    	System.out.print("Bienvenido al cliente de MonteCarlo.\nIndique que desea hacer:\n1. Añadir mas puntos a la aproximacion\n2. Resetear la aproximacion\n3. Salir\nOPCION: ");
+    	System.out.print("Bienvenido al cliente de MonteCarlo.\nIndique que desea hacer:\n1. Aï¿½adir mas puntos a la aproximacion\n2. Resetear la aproximacion\n3. Salir\nOPCION: ");
     	op = r.nextInt();
 
     	switch(op)
     	{
     		case 1:{
-    			System.out.print("Indique cuantos puntos desea añadir: ");
+    			System.out.print("Indique cuantos puntos desea aï¿½adir: ");
     			puntos = r.nextInt();
     			RefObRemoto.masPuntos(puntos);
     			break;
--- PCTR-Practicas/Practica11/src/practica11/iLibro.java
+++ PCTR-Practicas/Practica11/src/practica11/iLibro.java
@@ -1,10 +1,33 @@
 /**
- * @author David Moreno Del Valle
+ * Copyright (C) 2016 Jose Manuel Barba Gonzalez <zk at wordpress.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+package practica11;
+
+/**Fichero iLibro.java
+ * @author Jose Manuel Barba Gonzalez
+ * @version 1.0
+ * Programacion Concurrente y de Tiempo Real
+ * Area de CC. de la Computacion e I.A.
+ */
 
 import java.rmi.*;
-
+/**Descripcion
+ * 
+ */
 public interface iLibro extends Remote {
 
     public void insertar(Libro l) throws RemoteException;
