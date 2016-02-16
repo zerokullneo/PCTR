cf6ada03ca756a78dec9bb6e933a4f82541d571e
Jose M Barba Gonzalez <zerokullneo@localhost.localdomain>
2016-02-16 12:25:02 +0100

    Modificación de la práctica 12
    Corrección de errores 1.

--- PCTR-Practicas/Practica12/src/practica12/cPrimos.java
+++ PCTR-Practicas/Practica12/src/practica12/cPrimos.java
@@ -1,55 +1,83 @@
 /**
- * @author David Moreno Del Valle
+ * Copyright (C) 2016 Jose Manuel Barba Gonzalez <zk at wordpress.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+package practica12;
 
+/**Fichero cPrimos.java
+ * @author Jose Manuel Barba Gonzalez
+ * @version 1.0
+ * Programacion Concurrente y de Tiempo Real
+ * Area de CC. de la Computacion e I.A.
+ */
 
 import java.util.*;
 import java.net.*;
 import java.rmi.*;
 import java.text.*;
 
-public class cPrimos {
+/**Descripcion
+ * 
+ */
+public class cPrimos
+{
+     public static void main(String[]args)
+     {
+	try
+	{
+	     iPrimos OR1 = (iPrimos)Naming.lookup("//localhost/Servidor-1");
+	     iPrimos OR2 = (iPrimos)Naming.lookup("//localhost/Servidor-2");
+	     iPrimos OR3 = (iPrimos)Naming.lookup("//localhost/Servidor-3");
+	     iPrimos OR4 = (iPrimos)Naming.lookup("//localhost/Servidor-4");
 
-   public static void main(String[]args){
-    	try{
-	    	iPrimos OR1 = (iPrimos)Naming.lookup("//localhost/Servidor-1");
-	    	iPrimos OR2 = (iPrimos)Naming.lookup("//localhost/Servidor-2");
-	    	iPrimos OR3 = (iPrimos)Naming.lookup("//localhost/Servidor-3");
-	    	iPrimos OR4 = (iPrimos)Naming.lookup("//localhost/Servidor-4");
+	     int nNuc = Runtime.getRuntime().availableProcessors();
+	     Scanner leer = new Scanner(System.in);
+	     System.out.println("Introducir rango:");
+	     int linf = leer.nextInt();//Limite inferior del rango
+	     int lsup = leer.nextInt();//Limite superior del rango
+	     int Total = 0;
+	     int tbloque = (int)(lsup-linf)/nNuc;//tBloque sera el rango de cada hilo
+	     lsup = linf + tbloque;
 
-	    int nNuc = Runtime.getRuntime().availableProcessors();
-	    Scanner leer = new Scanner(System.in);
-      	System.out.println("Introducir rango:");
-      	int linf=leer.nextInt();//Limite inferior del rango
-      	int lsup=leer.nextInt();//Limite inferior del rango
-      	int Total = 0;
-      	int tbloque = (int)(lsup-linf)/nNuc;//tBloque sera el rango de cada hilo
-    	lsup = linf+tbloque;
+	     iPrimos [] v = new iPrimos[nNuc];
+	     v[0] = OR1;
+	     v[1] = OR2;
+	     v[2] = OR3;
+	     v[3] = OR4;
 
-    	iPrimos [] v = new iPrimos[nNuc];
+	     Date d = new Date();//Objeto de la clase Date
+	     DateFormat df = new SimpleDateFormat("HH:mm:ss:SSS");
+	     long inicCronom = System.currentTimeMillis();//se prepara el cronometro
+	     d.setTime(inicCronom);//activaciï¿½n del cronometro
 
-    	v[0] = OR1;
-    	v[1] = OR2;
-    	v[2] = OR3;
-    	v[3] = OR4;
+	     for(int i = 0; i < v.length; i++)
+	     {
+		Total += v[i].NumPrimos(linf,lsup);
+		linf=lsup;
+		lsup = lsup + tbloque;
+	     }
 
-    	Date d = new Date();//Objeto de la clase Date
-      	DateFormat df = new SimpleDateFormat("HH:mm:ss:SSS");
-      	long inicCronom = System.currentTimeMillis();//se prepara el cronometro
-      	d.setTime(inicCronom);//activación del cronometro
-
-		for(int i=0;i<v.length;i++){
-    		Total += v[i].NumPrimos(linf,lsup);
-    		linf=lsup;
-    		lsup = lsup+tbloque;
-    	}
-
-    	long finCronom = System.currentTimeMillis();//se detiene el cronometro
-      d.setTime(finCronom);
-      System.out.println("En el rango especificado hay "+Total+" numeros primos...");
-      System.out.println("Tiempo total de analisis:" + (finCronom - inicCronom) + " milisegundos");
-    }catch(Exception e){}
-
-}
+	     long finCronom = System.currentTimeMillis();//se detiene el cronometro
+	     d.setTime(finCronom);
+	     System.out.println("En el rango especificado hay " + Total + " numeros primos...");
+	     System.out.println("Tiempo total de analisis:" + (finCronom - inicCronom) + " milisegundos");
+	}
+	catch(Exception e)
+	{
+	     System.out.println("Error en el cliente cPrimos..." + e);
+	}
+     }
 }
--- PCTR-Practicas/Practica12/src/practica12/sPrimos.java
+++ PCTR-Practicas/Practica12/src/practica12/sPrimos.java
@@ -1,7 +1,28 @@
 /**
- * @author David Moreno Del Valle
+ * Copyright (C) 2016 Jose Manuel Barba Gonzalez <zk at wordpress.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+package practica12;
+
+/**Fichero sPrimos.java
+ * @author Jose Manuel Barba Gonzalez
+ * @version 1.0
+ * Programacion Concurrente y de Tiempo Real
+ * Area de CC. de la Computacion e I.A.
+ */
 
 import java.util.*;
 import java.rmi.registry.*;
@@ -9,52 +30,64 @@ import java.rmi.server.*;
 import java.rmi.*;
 import java.net.*;
 
-public class sPrimos extends UnicastRemoteObject implements iPrimos {
+/**Descripcion
+ * 
+ */
+public class sPrimos extends UnicastRemoteObject implements iPrimos
+{
+     public static int nTotal = 0;
+     public int li;//Limite inferior
+     public int ls;//Limite superior
 
-   public static int nTotal = 0;
-   public int li;//Limite inferior
-   public int ls;//Limite superior
+     public sPrimos() throws RemoteException
+     {
+	super();
+     }
 
-   public sPrimos() throws RemoteException {
-    	super();
-    }
+     public boolean esPrimo(int n) throws RemoteException
+     {
+	if(n <= 1)
+	     return(false);
 
-   public boolean esPrimo(int n) throws RemoteException
-   {
-   	if(n<=1)return(false);
-   	for(int i=2; i<=Math.sqrt(n);i++)
-   	  if(n%i==0)return(false);
-   	return(true);
-   }
+	for(int i = 2; i <= Math.sqrt(n); i++)
+	     if(n%i == 0)
+		return(false);
 
-   public int NumPrimos(int li, int ls) throws RemoteException{
-   	nTotal = 0;
-   	 for(int i=li;i<=ls;i++){
-    		if(esPrimo(i)){
-    			nTotal++;
-    		}
-      	}
-    	return nTotal;
+	return(true);
+     }
 
-   }
+     public int NumPrimos(int li, int ls) throws RemoteException
+     {
+	nTotal = 0;
 
-   public static void main(String[]args){
+	for(int i = li; i <= ls; i++)
+	     if(esPrimo(i))
+		nTotal++;
 
-    	try{
-    		iPrimos OR1 = new sPrimos();
-    		iPrimos OR2 = new sPrimos();
-    		iPrimos OR3 = new sPrimos();
-    		iPrimos OR4 = new sPrimos();
-    		Naming.bind("Servidor-1", OR1);
-    		System.out.println("Servidor 1 preparado");
-    		Naming.bind("Servidor-2", OR2);
-    		System.out.println("Servidor 2 preparado");
-    		Naming.bind("Servidor-3", OR3);
-    		System.out.println("Servidor 3 preparado");
-    		Naming.bind("Servidor-4", OR4);
-			System.out.println("Servidor 4 preparado");
-    	}catch (Exception e){
-    		System.out.println("Problemas en el servidor.");
-    	}
-    }
+	return nTotal;
+     }
+
+     public static void main(String[]args)
+     {
+	try
+	{
+	     iPrimos OR1 = new sPrimos();
+	     iPrimos OR2 = new sPrimos();
+	     iPrimos OR3 = new sPrimos();
+	     iPrimos OR4 = new sPrimos();
+
+	     Naming.bind("Servidor-1", OR1);
+	     System.out.println("Servidor 1 preparado");
+	     Naming.bind("Servidor-2", OR2);
+	     System.out.println("Servidor 2 preparado");
+	     Naming.bind("Servidor-3", OR3);
+	     System.out.println("Servidor 3 preparado");
+	     Naming.bind("Servidor-4", OR4);
+	     System.out.println("Servidor 4 preparado");
+	}
+	catch(Exception e)
+	{
+	     System.out.println("Error en el servidor sPrimos..." + e);
+	}
+     }
 }
