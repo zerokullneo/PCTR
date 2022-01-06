c2b4055e09fb00636f60e4c8d473ed265f8705c5
Jose M Barba <zerokullneo@gmail.com>
2022-01-06 01:10:47 +0100

    Práctica 2
    -Corrección en las fechas de creación.

--- PCTR-Practicas/practica2/src/practica2/Usa_tareaRunnable.java
+++ PCTR-Practicas/practica2/src/practica2/Usa_tareaRunnable.java
@@ -1,10 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2021 Jose Manuel Barba Gonzalez <zk at wordpress.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package practica2;
+
+/**Fichero Cuenta_Banca.java
+ * @author Jose Manuel Barba Gonzalez
+ * @version 1.0
+ * Programacion Concurrente y de Tiempo Real
+ * Area de CC. de la Computacion e I.A.
+ */
 
 /**Descripcion
  * Clase que simula las acciones recogidas por parámetro.
